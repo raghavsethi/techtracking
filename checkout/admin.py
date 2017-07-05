2b787b77ad3c93ca7d1e255c916dd46ee031fa35
Raghav Sethi <raghavsethi.rs@gmail.com>
2017-07-05 13:18:00 -0700

    Remove redundant user forms

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-
 from django import forms
 from django.contrib import admin
 from django.contrib.admin.widgets import AdminDateWidget
@@ -16,52 +14,6 @@ from checkout.models import *
 
 # Source:  https://medium.com/@ramykhuffash/django-authentication-with-just-an-email-and-password-no-username-required\
 # -33e47976b517
-class UserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ('email', )
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-# Source: https://docs.djangoproject.com/en/1.10/topics/auth/customizing/
-class UserChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = User
-        fields = ('email', 'password', 'is_active', 'is_staff', 'site')
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
-
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, ImportExportModelAdmin):
     resource_class = UserResource
@@ -76,10 +28,6 @@ class UserAdmin(BaseUserAdmin, ImportExportModelAdmin):
 
     actions = [send_welcome_email]
 
-    # The forms to add and change user instances
-    form = UserChangeForm
-    add_form = UserCreationForm
-
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
@@ -87,7 +35,7 @@ class UserAdmin(BaseUserAdmin, ImportExportModelAdmin):
     list_filter = ('is_staff', 'site')
     fieldsets = (
         (None, {'fields': ('email', 'password', 'name', 'site')}),
-        ('Permissions', {'fields': ('is_staff', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'user_permissions')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
