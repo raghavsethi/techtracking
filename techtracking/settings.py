900db7717f21c056a811de4df8027632b84d5617
Raghav Sethi <raghavsethi.rs@gmail.com>
2017-07-05 13:18:00 -0700

    Change to get SECRET_KEY from environment vars

--- techtracking/settings.py
+++ techtracking/settings.py
@@ -20,8 +20,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=0!wx82)cuf=pbd&5vwdu5=ov=pn1z%%dvd&6mts)ojcur8u*b'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
